02c44d9f3635fb79f024c6d546a6f795ef834b7e
rashadmustafa2244 <rashadmustafa2244@outlook.com>
2023-02-06 00:29:17 +0300

    Add project files.

---
+++ GYMLife/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GYMLife.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
